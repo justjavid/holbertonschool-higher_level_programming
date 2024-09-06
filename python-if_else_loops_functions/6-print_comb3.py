66804e117a7b3883097b9890a672a55ac74bf989
cavid <cavidagazad163@gmail.com>
2024-09-06 19:51:53 +0400

    printing different combinations of 2 digit numbers

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@ for x in range(100):
     if x == 89:
         print("{:02d}".format(x))
         break
-    print("{:02d}, ".format(x), end="")
+    if (x % 10) > x // 10:
+        print("{:02d}, ".format(x), end="")
