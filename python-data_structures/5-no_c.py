e1ad518a2f551fd338a0e429b44c7e07d6a918bc
9361@holbertonstudents.com <cavidagazad163@gmail.com>
2024-09-11 15:51:55 +0400

    removing all "c" characters from string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     a = list(my_string)
-    for i in a:
-        if i == 'c' or i == 'C':
-            a.remove(i)
+    for i in range(len(a)):
+        if a[i] == 'c' or a[i] == 'C':
+            a.remove(a[i])
     my_string = "".join(a)
     return my_string
