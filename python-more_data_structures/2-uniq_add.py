7afa34070d9444d2d143edbdc78c5bb3f0880658
cavid <cavidagazad163@gmail.com>
2024-09-15 17:52:14 +0400

    sum of unique integers

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = 0
+    uniq_list = []
+    for i in my_list:
+        if i not in uniq_list:
+            uniq_list.append(i)
+
+    for i in uniq_list:
+        sum += i
+    return sum
