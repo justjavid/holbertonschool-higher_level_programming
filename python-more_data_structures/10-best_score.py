aeb26e12239cf2e026ee9758571fff66d13ea579
cavid <cavidagazad163@gmail.com>
2024-09-15 18:41:28 +0400

    maximum value of dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    return (max(list(a_dictionary.values())))
+    for i in a_dictionary.keys():
+        if a_dictionary[i] == max(list(a_dictionary.values())):
+            return i
