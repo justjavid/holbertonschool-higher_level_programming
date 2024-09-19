3473268fba618dbce818c780d9679b7c83cf7966
cavid <cavidagazad163@gmail.com>
2024-09-19 19:20:36 +0400

    coordinates of square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,8 +5,8 @@
 class Square:
     """Create a private attribute and using getter and setter"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
