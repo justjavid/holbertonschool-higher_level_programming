2efc9d8089c3501bff81f88983d6d6b4d9d3e714
cavid <cavidagazad163@gmail.com>
2024-09-19 20:36:26 +0400

    coordinates of square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -38,7 +38,6 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__position[1]):
-                print("\n" * self.__position[1], end="")
+            print("\n" * self.__position[1], end='')
             for i in range(self.__size):
-                print(self.__position[0] * " " + self.__size * "#")
+                print(self.__position[0] * " " + self.size * "#")
