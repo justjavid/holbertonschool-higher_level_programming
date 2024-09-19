7a9b8b34052990156e04a32b78d3eda1add1129d
cavid <cavidagazad163@gmail.com>
2024-09-19 17:18:52 +0400

    size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for creating empty class"""
+
+
+class Square:
+    """Create a private attribute and raising error when needed"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
