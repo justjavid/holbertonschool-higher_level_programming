b83c9dbb0dc5ed5c71c6199c13da967df8fd4e43
cavid <cavidagazad163@gmail.com>
2024-09-19 17:29:53 +0400

    public instance method

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for creating empty class"""
+
+
+class Square:
+    """Create a private attribute and raising error when needed"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+        def area(self):
+            return self.__size ** 2
