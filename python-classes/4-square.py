a31e213f757f142f19d7c62ef7a004e9d8b1efd3
cavid <cavidagazad163@gmail.com>
2024-09-19 18:05:30 +0400

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Create a private attribute and using getter and setter"""
     def __init__(self, size=0):
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
@@ -17,6 +17,7 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
