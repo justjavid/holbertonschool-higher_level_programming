6acd9368e7ab19a4f73558f3ac9a5d41136b74ee
cavid <cavidagazad163@gmail.com>
2024-09-25 13:11:49 +0400

    print the list sorted in class that inherited from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module containing print_sorted func"""
+
+
 class MyList(list):
+    """Class inherited from list class"""
+
     def print_sorted(self):
         print(sorted(self))
