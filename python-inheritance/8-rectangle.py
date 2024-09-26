84ff5fcd74adc81ed3e859eb75fa170e3e4d874f
cavid <cavidagazad163@gmail.com>
2024-09-26 16:51:22 +0400

    rectangle with height and width

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,18 +11,3 @@ class Rectangle(BaseGeometry):
         self.__height = height
         Rectangle.integer_validator(self, width, self.__width)
         Rectangle.integer_validator(self, height, self.__height)
-
-r = Rectangle(3, 5)
-
-print(r)
-print(dir(r))
-
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
