7646ca09efd5547bfd5f37440a1b6f4e38612f5d
cavid <cavidagazad163@gmail.com>
2024-09-26 16:21:28 +0400

    exception message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module containing BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
