58549d51734f9ebb90ce536e522340e59c2b4605
cavid <cavidagazad163@gmail.com>
2024-09-26 16:38:44 +0400

    inherit class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,3 +13,9 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("%s must be greater than 0" % (name))
 
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        Rectangle.integer_validator(self.__width, width)
+        Rectangle.integer_validator(self.__height, height)
+        self.__width = width
+        self.__height = height
