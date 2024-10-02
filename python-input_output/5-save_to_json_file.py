42798dd7bc5323a8916ff9daa9189bda1494d1a3
Javid <cavidagazad163@gmail.com>
2024-10-02 17:20:58 +0400

    Loading from JSON file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,3 +7,4 @@ def save_to_json_file(my_obj, filename):
     """Function for saving my_obj as JSON string to file"""
     with open(filename, 'w', encoding="utf-8") as f:
         json.dump(my_obj, f)
+
