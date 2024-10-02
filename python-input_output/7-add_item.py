4e6f8c70cb29dcb6ddc545811595259a8ada6703
Javid <cavidagazad163@gmail.com>
2024-10-02 17:57:46 +0400

    Loading JSON file, adding new objects as JSON string and saving objects in JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+"""Module for file"""
 import sys
-import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
