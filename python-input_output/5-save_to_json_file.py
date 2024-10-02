43952f4162678594b07275e92f496a1b1f0a4eb7
Javid <cavidagazad163@gmail.com>
2024-10-02 16:52:08 +0400

    saving object as JSON string to file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ def save_to_json_file(my_obj, filename):
     """Function for saving my_obj as JSON string to file"""
     with open(filename, 'a', encoding="utf-8") as f:
         obj = json.dump(my_obj, f)
-        f.write(str(obj))
+        f.write(obj)
