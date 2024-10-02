feaec53a1140e32d63a09d72797d9e1b0f4f49b4
Javid <cavidagazad163@gmail.com>
2024-10-02 15:39:54 +0400

    Reading text and printing it to stdout

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+"""Function for reading text file"""
 def read_file(filename=""):
-    """Function for reading text file"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(f"{read_data}")
