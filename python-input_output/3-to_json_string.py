4698b41f479b92a1f190a77304d0e2da8a70067c
Javid <cavidagazad163@gmail.com>
2024-10-02 16:05:17 +0400

    JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""Module for file"""
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of my_obj"""
+    return json.dumps(my_obj)
