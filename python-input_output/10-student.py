7e9356ffa0d40d71fb5ce32d6e4b37e20f6c79d1
cavid <cavidagazad163@gmail.com>
2024-10-03 19:14:35 +0400

    Filtering class object before to JSON

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for class"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_dict = {}
+        if not attrs:
+            return self.__dict__
+        for key in attrs:
+            if key in self.__dict__.keys():
+                new_dict[key] = self.__dict__[key]
+        return new_dict
