d25b84cc27f2592dc70b53767c13a60d98a484cb
cavid <cavidagazad163@gmail.com>
2024-10-03 19:55:48 +0400

    Pascal's Triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for Pascal's triangle"""
+from logging import lastResort
 
 
 def pascal_triangle(n):
@@ -7,8 +8,9 @@ def pascal_triangle(n):
     the Pascal's triangle of n"""
     if n <= 0:
         return []
-    triangle = []
-    for num in range(0, n):
-        res = [int(x) for x in str(pow(11, num))]
+    triangle = [[1], [1, 1]]
+    for num in range(2, n):
+        last = triangle[num - 1]
+        res = [1] + [last[i] + last[i + 1] for i in range(len(last) - 1)] + [1]
         triangle.append(res)
     return triangle
