f9e89ea7d36902e6f807b6cec9631492f3ddb066
cavid <cavidagazad163@gmail.com>
2024-10-03 18:00:30 +0400

    Converting JSON string to dictionary

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Module for file"""
-import json
 
 
 def class_to_json(obj):
     """Function convert JSON str to dict"""
-    return json.loads(json.dumps(obj.__dict__))
+    return obj.__dict__
