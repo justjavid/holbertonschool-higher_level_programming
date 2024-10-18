500160c2d1036383276c825e8b60756014e66d74
justjavid <cavidagazad163@gmail.com>
2024-10-18 21:24:49 +0400

    fix error

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -12,11 +12,10 @@ def fetch_and_print_posts():
 def fetch_and_save_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
     if r.status_code == 200:
-        data = r.json()
-    with open('posts.csv', 'w', newline='') as file:
-        fieldnames = ['id', 'title', 'body']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        for user in data:
-            writer.writerow({key: value for key, value in user.items() if key != 'userId'})
-fetch_and_print_posts()
+        with open('posts.csv', 'w', newline='') as file:
+            data = r.json()
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            for user in data:
+                writer.writerow({key: value for key, value in user.items() if key != 'userId'})
