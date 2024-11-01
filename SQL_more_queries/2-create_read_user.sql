93cdfe66030d437f9031a6022947afaaa9e6c553
justjavid <cavidagazad163@gmail.com>
2024-11-01 21:42:26 +0000

    create user and database , grant only select privilege to user on this database

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Creating MySQL server user user_0d_2 and database hbtn_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
