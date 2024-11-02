83f497a90e7aa3749ce22c85e1c07b70e3e7320b
justjavid <cavidagazad163@gmail.com>
2024-11-02 12:22:55 +0000

    writing with JOIN instead of subquery

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,4 @@
 -- listing all genres of the show Dexter
-SELECT g.name name FROM tv_show_genres sg INNER JOIN tv_genres g ON sg.genre_id = g.id
-WHERE sg.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter') ORDER BY g.name;
+SELECT g.name name FROM (tv_show_genres sg INNER JOIN tv_genres g ON sg.genre_id = g.id) 
+INNER JOIN tv_shows s ON s.id = sg.show_id
+WHERE s.title = 'Dexter' ORDER BY g.name;
