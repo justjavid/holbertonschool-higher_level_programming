4f1c5d52ef8e2e28730461aa0a17b39c3e20916b
justjavid <cavidagazad163@gmail.com>
2024-11-03 16:56:25 +0000

    problem fixed

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Module for Selecting states"""
 
-
 from sys import argv
 import MySQLdb
 
-db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-cur = db.cursor()
-
-cur.execute("SELECT * FROM states")
-
-for state in cur.fetchall():
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for state in cur.fetchall():
         print(state)
+    cur.close()
+    db.close()
+
