b4b8158cde58133b310e9271149ea7abb3c012a6
justjavid <cavidagazad163@gmail.com>
2024-11-03 17:45:48 +0000

    takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for Selecting cities"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT c.id, c.name, s.name \
+                    FROM cities c \
+                    INNER JOIN states s ON s.id = c.state_id ")
+    for city in cur.fetchall():
+        if city[1] == argv[4]:
+            print(city[0], end='')
+    cur.close()
+    db.close()
