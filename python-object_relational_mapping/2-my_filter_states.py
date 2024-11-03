1c5578f46ca16a3af3f19e1da7f776fb8311edc4
justjavid <cavidagazad163@gmail.com>
2024-11-03 17:23:36 +0000

    fix problem

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,10 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY 'name' = %s ORDER BY id"
+    query = "SELECT * \
+            FROM 'states' \
+            WHERE BINARY 'name' = %s \
+            ORDER BY id"
     cur.execute(query, (argv[4],))
     for state in cur.fetchall():
         print(state)
